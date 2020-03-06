b5d606c7fde8ae62d10d709249da1e7a46fa23e0
Jimmy Mena <jmeliodas96>
2020-03-06 11:40:19 -0600

    add feauture endpoint and add jsx

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import './index.css';
+
 
 export default class Product extends Component {
     render() {
         const {id, name, description, price, imageName} = this.props.product;
         let imageNameLocation = "img/" + imageName
         return (
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
-                
+            /** 
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">       
                 <div className="card">
                     <div className="img-container p-5" onClick={console.log("you clicked me")}>
                         <Link to="/details">
@@ -18,10 +20,29 @@ export default class Product extends Component {
                     </div>
                 </div>
             </ProductWrapper>
+            **/
+           <div id="productsWrapper" className="row">
+               <div className="col-sm-3">
+                   <a id={id} title={name}>
+                       <div className="productInfo">
+                           <h3>{name}</h3>
+                           <img className="product-image img-fluid img-thumbnail" src={imageNameLocation} alt="Product" />
+                           <p className="description">{description}</p>
+                       </div>
+                   </a>
+                   <div className="action">
+                       <p className="price float-left">${price}</p>
+                       <a className="btn btn-sm btn-danger order-button float-right" id={id} title={name}>Order Now</a>
+                   </div>
+               </div>
+           </div>
+
         )
     }
 }
 
-const ProductWrapper = styled.nav`
 
+/*
+const ProductWrapper = styled.nav`
 `;
+*/
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react'
 import Product from "./Product";
 import Title from './Title';
+import axios from 'axios';
 import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
 
+    state = {
+        feature:[]
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/feature')
+        .then(response => {
+        console.log(response.data);
+        this.setState({
+            feature:response.data
+        });
+        })
+        .catch(error => {
+        console.log(error);
+        });
+    }
+
 
     render() {
+        const { id, name, description, price, imageName } = this.state.feature;
+        let imageNameLocation = "img/" + imageName;
+
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                     <div className="row">
-                        <Title name="Bienvenido a" title="Fourth Coffe"/>
+                        <Title name="Welcome to " title="Fourth Coffee!"/>
+
+                        <div id="featuredProduct" className="row"> 
+                            <div className="col-sm-8">
+                                <img alt="Featured Product" src={imageNameLocation} className="img-fluid rounded"/>
+                                </div>
+                                <div id="featuredProductInfo" className="col-sm-4">
+                                <div id="productInfo">
+                                <h2>{name}</h2>
+                                <p className="price">${price}</p>
+                                <p className="description">{description}</p>
+                                </div>
+                                <div id="callToAction">
+                                <a className="btn btn-danger order-button" id={id} title={name}>Order Now</a>
+                                </div>
+                                </div>
+                            </div>
                         <div className="row">
                             <ProductConsumer>
                                  {(value) => {
@@ -22,6 +59,9 @@ export default class ProductList extends Component {
                                  }}
                             </ProductConsumer>
                         </div>
+
+
+                        
                     </div>
                     </div>
                 </div>
